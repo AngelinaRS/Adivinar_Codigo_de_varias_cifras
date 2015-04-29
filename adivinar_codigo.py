0bf7bcc2769366fed83a1a70fcdc2ed62277a51f
AngelinaRS <candy26y_@hotmail.com>
2015-04-29 10:55:44 -0600

    Documenté y corregí ortografía

--- adivinar_codigo.py
+++ adivinar_codigo.py
@@ -3,24 +3,28 @@ import random   #módulo aleatorio
 
 print "Bienvenido al juego de MasterMind \n"
 print "Consiste en adivinar un código de la cantidad de cifras que quieras \n"
-print "El máximo son 7 \n"
-print "No te preocupes que tus vidas son ilimitadas"
+print "El máximo son 10 \n"
+print "No te preocupes que tus vidas son ilimitadas \n"
 
-cantidad_de_codigo=input("Escoge de cuántas cifras quieres el código que adivinarás: \n\n")
-print "Muy Bien! Tú código tendrá %s cifras diferentes \n\n"% cantidad_de_codigo
+cantidad_de_codigo=input("Escoge de cuántas cifras quieres el código que adivinarás:  ") 
+if cantidad_de_codigo==1:
+	print "Aaaah que fácil tu código es de una cifra"
+else:
+	print "\nMuy Bien! Tú código tendrá %s cifras diferentes \n\n"% cantidad_de_codigo
 codigo=["0","1","2","3","4","5","6","7","8","9"]
 codigo_a_adivinar=""
 
-for num in range(cantidad_de_codigo):
-	codigo_generado= random.choice(codigo)
-	while codigo_generado in codigo_a_adivinar:
+for num in range(cantidad_de_codigo): #itera en la respuesta del usuario
+	codigo_generado= random.choice(codigo) #se genera un numero aleatorio
+	while codigo_generado in codigo_a_adivinar: #Evita que se repitan numeros
 		codigo_generado=random.choice(codigo)
-	codigo_a_adivinar=codigo_a_adivinar+ codigo_generado
-print codigo_a_adivinar
+	codigo_a_adivinar=codigo_a_adivinar + codigo_generado
+
+print codigo_a_adivinar 
 
 
 
-propuesta= raw_input("Que codigo propones?  ")
+propuesta= raw_input("\nQue código propones?  ")
 
 intentos=1
 while propuesta != codigo_a_adivinar and propuesta!="*":
@@ -28,19 +32,19 @@ while propuesta != codigo_a_adivinar and propuesta!="*":
 	aciertos= 0
 	coincidencias=0
 	for i in range(cantidad_de_codigo):
-		if propuesta[i] == codigo_a_adivinar[i]:
+		if propuesta[i] == codigo_a_adivinar[i]: #Si en la propuesta del usuario acierta en la posición de algunos números
 			aciertos= aciertos+1
-		elif propuesta[i] in codigo_a_adivinar:
+		elif propuesta[i] in codigo_a_adivinar: #Si la propuesta del usuario tiene números que coinciden pero no estan en la posición correcta
 			coincidencias=coincidencias+1
-	print "Tu codigo tiene %d aciertos y %d coincidencias"%(aciertos,coincidencias)
+	print "\nTu código tiene %d aciertos y %d coincidencias"%(aciertos,coincidencias)
 
-	propuesta=raw_input("Propon otro codigo:  ")
+	propuesta=raw_input("\nPropón otro código o (* para rendirte)  ")
 
-if propuesta=="*":
-	print "El codigo era %s" % codigo_a_adivinar 
-	print "Suerte la proxima vez"
+if propuesta=="*": #Una opción para que el usuario se rinda
+	print "\nEl código era: %s" % codigo_a_adivinar 
+	print "\nSuerte la próxima vez"
 else:
 	if intentos==1:
-		print "Felicitaciones adivinaste el codigo en %d intento"% intentos
+		print "\nFelicitaciones adivinaste el código en %d intento"% intentos
 	else:
-		print "Felicitaciones adivinaste el codigo en %d intentos"% intentos
+		print "\n”Felicitaciones adivinaste el código en %d intentos"% intentos
